335ce6497086be7491f5dd69de63ca5a160a965b
sumit212-dev <132038818+sumit2221-div@users.noreply.github.com>
2025-03-08 15:29:05 +0530

    Update cookie options to enforce secure and sameSite settings for login and logout

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -1,3 +1,48 @@
+import express from 'express';
+import ConnectDB from './src/db/database.js';
+import dotenv from 'dotenv';
+import cors from 'cors'; 
+import authRoute from "./src/routes/user.routes.js";
+import workoutRoute from "./src/routes/workout.route.js";
+import DietRoute from "./src/routes/diet.router.js";
+import NutritionRoute from "./src/routes/nutrition.router.js";
+import cookieParser from 'cookie-parser';
+
+const app = express();
+dotenv.config({ path: './.env' });
+
+// Middleware to parse JSON bodies
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// Configure CORS
+const allowedOrigins = ["https://fit-gen.netlify.app", "http://localhost:5173"];
+app.use(cors({ 
+    origin: allowedOrigins,  // Allow both production and development URLs
+    credentials: true 
+}));
+
+// Define a route for GET requests to "/"
+app.get("/", (req, res) => {
+    res.send("Welcome to the Express server!");
+});
+
+ConnectDB();
+
+// Define routes
+app.use("/api/auth", authRoute);
+app.use("/api/workout", workoutRoute);
+app.use("/api/diet", DietRoute);
+app.use("/api/nutrition", NutritionRoute);
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
 import { User } from "../model/user.model.js";
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
@@ -59,9 +104,9 @@ export const LoginUser = async (req, res) => {
 
     const cookieOptions = {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-      sameSite: 'None', // Required for cross-origin cookies
-      path: "/",
+      secure: true, // Use secure cookies in production
+      sameSite: 'none', 
+    
     };
 
     // Set cookies
@@ -91,9 +136,8 @@ export const LogoutUser = async (req, res) => {
 
     const cookieOptions = {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-      sameSite: 'None',
-      path: "/",
+      secure: true, // Use secure cookies in production
+      sameSite: 'none', 
     };
 
     res.clearCookie('accessToken', cookieOptions);
